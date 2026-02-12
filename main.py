c6ff5db8c2aae227d225e7cc84f2c99c46686161
joeottofaro <joseph.ottofaro@gmail.com>
2026-02-12 12:45:54 +0000

    Updated to handle KeyError

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-user_word = input("Enter a word: ").upper()
-phonetic_code = [new_dict[letter] for letter in user_word]
-print(phonetic_code)
+
+def code():
+    user_word = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [new_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        code()
+    else:
+        print(phonetic_code)
+
+
+code()
